aff47808f0d6eeac0401ff68f35c9194c36389d6
Sebastien Van Cauwenberghe <svancau@gmail.com>
2013-10-18 22:16:37 +0200

    Fixed license header for NameCheckListener

--- src/eu/mindspark/fhdl/NameCheckListener.java
+++ src/eu/mindspark/fhdl/NameCheckListener.java
@@ -1,3 +1,20 @@
+/*  FHDL Antlr 4 Grammar for VHDL IEEE Std 1076-1993 parsing
+    Copyright (C) 2013 Stanislas Rusinsky, Sebastien Van Cauwenberghe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package eu.mindspark.fhdl;
 
 import org.antlr.v4.runtime.misc.NotNull;
