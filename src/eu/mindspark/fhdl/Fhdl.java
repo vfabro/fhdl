95298284ecdc8dce670bad0685e7881c55894001
Sebastien Van Cauwenberghe <svancau@gmail.com>
2013-10-18 22:08:23 +0200

    Add multiple files parsing support

--- src/eu/mindspark/fhdl/Fhdl.java
+++ src/eu/mindspark/fhdl/Fhdl.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.*;
+import eu.mindspark.fhdl.*;
 
 public class Fhdl {
 
 	public Fhdl() {}
 
 	public static void main(String[] args) throws IOException {
-		fhdlLexer lexer = new fhdlLexer(new ANTLRFileStream(args[0]));
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		fhdlParser p = new fhdlParser(tokens);
-		p.setBuildParseTree(true);
-		p.addParseListener(new SemanticFhdlListener());
-		ParserRuleContext t = p.design_file();
+		for (String arg : args) {
+			System.out.println("Analyzing "+arg);
+			fhdlLexer lexer = new fhdlLexer(new ANTLRFileStream(arg));
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			fhdlParser p = new fhdlParser(tokens);
+			System.gc(); // Cleanup
+		}
 	}
 }
